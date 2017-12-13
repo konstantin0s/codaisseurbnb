a3bd62725eaee8eb6ae5f6187f0a8370c912f2df
konstantin0s <constantintofan85@gmail.com>
2017-12-13 07:13:24 +0100

    One-to-Many: Models

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-         has_many :rooms, dependent: :destroy
+         has_many :events, dependent: :destroy
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+#User.destroy_all
+diana = User.create!(
+  email: 'diana@codaisseur.com',
+  password: '123456'
+)
+
+Event.create!(
+  name: "Charity",
+  description: "Charity for third world country.",
+  capacity: 200,
+  location: "Colon 58, 1B, 11100 Cadiz (Spain)",
+  includes_food: false,
+  includes_drinks: true,
+  starts_at: 2,
+ends_at: 3,
+  price: 100,
+  active: true,
+  user: diana)
+)
